6a5382cd2c6ad145e8ec8dc971910082ac612d1f
Raul Alencar <126506918+raulalen@users.noreply.github.com>
2024-08-19 20:52:32 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace CalculadoraNota
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtnota4 = new System.Windows.Forms.TextBox();
+            this.lblnota4 = new System.Windows.Forms.Label();
+            this.txtnota3 = new System.Windows.Forms.TextBox();
+            this.lblnota3 = new System.Windows.Forms.Label();
+            this.txtnota2 = new System.Windows.Forms.TextBox();
+            this.lblnota2 = new System.Windows.Forms.Label();
+            this.btncalcularmedia = new System.Windows.Forms.Button();
+            this.txtnota1 = new System.Windows.Forms.TextBox();
+            this.lblnota1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtnota4
+            // 
+            this.txtnota4.Location = new System.Drawing.Point(77, 133);
+            this.txtnota4.Name = "txtnota4";
+            this.txtnota4.Size = new System.Drawing.Size(107, 20);
+            this.txtnota4.TabIndex = 18;
+            // 
+            // lblnota4
+            // 
+            this.lblnota4.AutoSize = true;
+            this.lblnota4.Location = new System.Drawing.Point(32, 136);
+            this.lblnota4.Name = "lblnota4";
+            this.lblnota4.Size = new System.Drawing.Size(39, 13);
+            this.lblnota4.TabIndex = 17;
+            this.lblnota4.Text = "Nota 4";
+            // 
+            // txtnota3
+            // 
+            this.txtnota3.Location = new System.Drawing.Point(77, 97);
+            this.txtnota3.Name = "txtnota3";
+            this.txtnota3.Size = new System.Drawing.Size(107, 20);
+            this.txtnota3.TabIndex = 16;
+            // 
+            // lblnota3
+            // 
+            this.lblnota3.AutoSize = true;
+            this.lblnota3.Location = new System.Drawing.Point(32, 100);
+            this.lblnota3.Name = "lblnota3";
+            this.lblnota3.Size = new System.Drawing.Size(39, 13);
+            this.lblnota3.TabIndex = 15;
+            this.lblnota3.Text = "Nota 3";
+            // 
+            // txtnota2
+            // 
+            this.txtnota2.Location = new System.Drawing.Point(77, 61);
+            this.txtnota2.Name = "txtnota2";
+            this.txtnota2.Size = new System.Drawing.Size(107, 20);
+            this.txtnota2.TabIndex = 14;
+            // 
+            // lblnota2
+            // 
+            this.lblnota2.AutoSize = true;
+            this.lblnota2.Location = new System.Drawing.Point(32, 64);
+            this.lblnota2.Name = "lblnota2";
+            this.lblnota2.Size = new System.Drawing.Size(39, 13);
+            this.lblnota2.TabIndex = 13;
+            this.lblnota2.Text = "Nota 2";
+            // 
+            // btncalcularmedia
+            // 
+            this.btncalcularmedia.Location = new System.Drawing.Point(77, 187);
+            this.btncalcularmedia.Name = "btncalcularmedia";
+            this.btncalcularmedia.Size = new System.Drawing.Size(92, 37);
+            this.btncalcularmedia.TabIndex = 12;
+            this.btncalcularmedia.Text = "Calcular Média";
+            this.btncalcularmedia.UseVisualStyleBackColor = true;
+            this.btncalcularmedia.Click += new System.EventHandler(this.btncalcularmedia_Click);
+            // 
+            // txtnota1
+            // 
+            this.txtnota1.Location = new System.Drawing.Point(77, 24);
+            this.txtnota1.Name = "txtnota1";
+            this.txtnota1.Size = new System.Drawing.Size(107, 20);
+            this.txtnota1.TabIndex = 11;
+            // 
+            // lblnota1
+            // 
+            this.lblnota1.AutoSize = true;
+            this.lblnota1.Location = new System.Drawing.Point(32, 27);
+            this.lblnota1.Name = "lblnota1";
+            this.lblnota1.Size = new System.Drawing.Size(39, 13);
+            this.lblnota1.TabIndex = 10;
+            this.lblnota1.Text = "Nota 1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(253, 246);
+            this.Controls.Add(this.txtnota4);
+            this.Controls.Add(this.lblnota4);
+            this.Controls.Add(this.txtnota3);
+            this.Controls.Add(this.lblnota3);
+            this.Controls.Add(this.txtnota2);
+            this.Controls.Add(this.lblnota2);
+            this.Controls.Add(this.btncalcularmedia);
+            this.Controls.Add(this.txtnota1);
+            this.Controls.Add(this.lblnota1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtnota4;
+        private System.Windows.Forms.Label lblnota4;
+        private System.Windows.Forms.TextBox txtnota3;
+        private System.Windows.Forms.Label lblnota3;
+        private System.Windows.Forms.TextBox txtnota2;
+        private System.Windows.Forms.Label lblnota2;
+        private System.Windows.Forms.Button btncalcularmedia;
+        private System.Windows.Forms.TextBox txtnota1;
+        private System.Windows.Forms.Label lblnota1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculadoraNota
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalcularmedia_Click(object sender, EventArgs e)
+        {
+            double nota1, nota2, nota3, nota4, media;
+            nota1 = Convert.ToInt16(txtnota1.Text);
+            nota2 = Convert.ToInt16(txtnota2.Text);
+            nota3 = Convert.ToInt16(txtnota3.Text);
+            nota4 = Convert.ToInt16(txtnota4.Text);
+            media = (nota1 + nota2 + nota3 + nota4) / 4;
+            MessageBox.Show("O Resultado é: " + media);
+        }
+    }
+}
